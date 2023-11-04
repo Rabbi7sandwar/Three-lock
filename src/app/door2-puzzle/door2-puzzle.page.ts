fe78bd2da77e45e6e6d7d42d25b8e934eb382587
Rabbi7sandwar <raabisandwarsandwar@gmail.com>
2023-11-04 13:54:39 +0530

    memory game

--- src/app/door2-puzzle/door2-puzzle.page.ts
+++ src/app/door2-puzzle/door2-puzzle.page.ts
@@ -7,16 +7,31 @@ import { AlertController, ModalController } from '@ionic/angular';
   styleUrls: ['./door2-puzzle.page.scss'],
 })
 export class Door2PuzzlePage implements OnInit {
-  displayFigure = ''
-  showBox = 'showBox'
+  firstSet = [4, 2, 4, 3]
+  secondSet = [1, 2, 2, 4, 5]
+  thirdSet = [5, 4, 3, 1, 1, 5, 2]
+  currentIndex = 0;
+  currentElement: number | null = null;
 
-  constructor(private modalCtrl: ModalController,private alertController: AlertController) { }
+  constructor(private modalCtrl: ModalController, private alertController: AlertController) { }
 
 
   ngOnInit() {
-    setTimeout(() => {
-      this.displayFigure = 'show'
-    }, 5000);
+    this.displayElementsAtInterval();
+  }
+
+  displayElementsAtInterval() {
+    const interval = 1000;
+    const displayNextElement = () => {
+      if (this.currentIndex < this.firstSet.length) {
+        this.currentElement = this.firstSet[this.currentIndex];
+        this.currentIndex++;
+        setTimeout(displayNextElement, interval);
+      } else {
+        this.currentElement = null;
+      }
+    };
+    displayNextElement();
   }
 
   cancel() {
